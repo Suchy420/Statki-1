f557c319e081694deeadfcc28d77b753d594afb3
Suchy420 <94827103+Suchy420@users.noreply.github.com>
2021-12-13 19:55:37 +0100

    Update Program.cs

--- Statki/Program.cs
+++ Statki/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+using BattleShipConsole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
 
-namespace Statki
+namespace BattleShipConsole
 {
-    class Program
-    {
-        static void Main(string[] args)
+class Program
+{
+static void Main(string[] args)
+{
+
+        bool pokazstatki = false;//pokazuje pozycje wrogie
+
+        zasoby mzasoby = new zasoby();
+        zasoby pzasoby = new zasoby();
+
+
+
+        Dictionary<char, int> kordy = PopulateDictionary();
+        naglowek();
+        for (int h = 0; h < 19; h++)
         {
-            Console.WriteLine("Hello World!");
+            Write(" ");
         }
-    }
-}
+
+
+        PrintMap(mzasoby.pozstrzalu, mzasoby, pzasoby, pokazstatki);
